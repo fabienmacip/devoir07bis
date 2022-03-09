2825d5bbdf316377cee9a45791c906cd4752b82a
fabienmacip <fabien.macip@gmail.com>
2022-03-09 18:33:24 +0100

    CSS OK, reste à faire BURGER-MENU

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,6 +6,11 @@ export default function Footer({isLight, handleToggleTheme}) {
       <button onClick={handleToggleTheme}>
         Switch to {isLight ? "dark" : "light"} theme
       </button>
+      <Socials>
+        <img src="/uploads/facebook.png" alt="Facebook" id="logo-facebook" className="logos-reseaux-sociaux"/>
+        <img src="/uploads/instagram.png" alt="Instagram" id="logo-instagram" className="logos-reseaux-sociaux"/>
+      </Socials>
+
     </Wrapper>
   )
 }
@@ -17,4 +22,17 @@ const Wrapper = styled.footer`
   align-items: center;
   /*border-top : solid 1px;*/
   background-color: ${(props) => props.theme.mainColor};
+
+  @media(max-width: 380px) {
+    & button {
+      max-width: 120px;
+    }
+  }
+`;
+
+const Socials = styled.div`
+margin-left : 2rem;
+& img {
+  margin-left: 1rem;
+}
 `;
